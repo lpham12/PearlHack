ab290d17328bdbe9b59e23eaecbbd04c3267bbbd
Marisol Garcia <garciam0410@gmail.com>
2019-02-17 08:53:45 -0800

    resolved merge conflicts

--- Pearl_Hack/public/app.js
+++ Pearl_Hack/public/app.js
@@ -1,4 +1,29 @@
-//function to log in user
+
+  // Initialize Firebase
+  // TODO: Replace with your project's customized code snippet
+  var config = {
+     apiKey: "AIzaSyAPZ-lHN-0djIAvdie40OAHLzhox2UilMI",
+     authDomain: "pearlhack-6998a.firebaseapp.com",
+     databaseURL: "https://pearlhack-6998a.firebaseio.com",
+     projectId: "pearlhack-6998a",
+     storageBucket: "pearlhack-6998a.appspot.com",
+     messagingSenderId: "930675368553"
+  };
+  firebase.initializeApp(config);
+// Firebase App is always required and must be first
+/*var firebase = require("firebase/app");
+
+ Add additional services that you want to use
+require("firebase/auth");
+require("firebase/database");
+require("firebase/firestore");
+require("firebase/messaging");
+require("firebase/functions");
+*/
+// Comment out (or don't require) services that you don't want to use
+// require("firebase/storage");
+firebase.initializeApp(config);
+/*
 function googleLogin() {
     var provider = new firebase.auth.GoogleAuthProvider();
     
@@ -38,3 +63,104 @@ function googleLogin() {
         // ...
       });
 }
+*/
+var usersRef = firebase.database().ref('user');
+
+document.getElementById('quizForm').addEventListener('submit', submitForm);
+
+function submitForm(e){
+  e.preventDefault();
+
+  var gender = getInputVal('gender');
+
+  saveMessage(gender);
+
+  alert("Alert Uploaded to Server");
+
+  document.getElementById('quizForm').reset();
+}
+
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(color){
+  var newUsersRef = usersRef.push();
+  newUsersRef.set({
+    gender: gender,
+    
+  });
+}
+/*
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+  apiKey: "AIzaSyAPZ-lHN-0djIAvdie40OAHLzhox2UilMI",
+    authDomain: "pearlhack-6998a.firebaseapp.com",
+    databaseURL: "https://pearlhack-6998a.firebaseio.com",
+    projectId: "pearlhack-6998a",
+    storageBucket: "pearlhack-6998a.appspot.com",
+    messagingSenderId: "930675368553"
+});
+ 
+
+var config = {
+  apiKey: "AIzaSyAPZ-lHN-0djIAvdie40OAHLzhox2UilMI",
+    authDomain: "pearlhack-6998a.firebaseapp.com",
+    databaseURL: "https://pearlhack-6998a.firebaseio.com",
+    projectId: "pearlhack-6998a",
+    storageBucket: "pearlhack-6998a.appspot.com",
+    messagingSenderId: "930675368553"
+};
+firebase.initializeApp(config);
+var users = firebase.database().ref("users");
+
+
+var submitUser = function () {
+
+  // Get input values from each of the form elements
+  var firstName = $("#firstName").val();
+  console.log(firstName);
+  var lastName = $("#lastName").val();
+  console.log(lastName);
+
+  // Push a new recommendation to the database using those values
+  users.push({
+    "firsName": firstName,
+    "lastName": lastName,
+  });
+};
+
+
+
+
+firebase.auth().onAuthStateChanged(function(user){
+  if(user) {
+document.getElementById("quizForm").style.display = "initial";
+document.getElementById("quizForm").style.display = "none";
+} else {
+
+  }
+})
+
+function submit(){
+  var data= [];
+
+  //new connection to database
+  ref.on("value", function (snapshot) {
+    console.log(snapshot.val());
+
+    data=snapshot.val();
+  })
+
+    $('#quizFrom').submit(function(event){
+      var $form = $(this);
+      console.log("form submitted to database");
+    })
+
+    var firstName = $('#firstName').val();
+    console.log(firstName);
+
+    var lastName = $('#lastName').val();
+    console.log(lastName);
+}*/
